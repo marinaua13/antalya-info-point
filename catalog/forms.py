b419d9ba39fc9c5b4715475ce69f6d8492d2c5f0
Marina <marina.ua13@gmail.com>
2024-04-06 00:17:15 +0300

    update forms.py: AuthorForm

--- catalog/forms.py
+++ catalog/forms.py
@@ -1,39 +1,12 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-
-from .models import Author, Commentary
-
-
-class AuthorsForm(forms.ModelForm):
-    picture = forms.ImageField(
-        required=False
-    )  # Додаємо поле для завантаження фотографії
-
-    class Meta:
-        model = Author
-        fields = ["email", "mobile_phone", "years_of_experience"]
-
-    def __init__(self, *args, **kwargs):
-        super(AuthorsForm, self).__init__(*args, **kwargs)
-        self.fields["picture"].widget.attrs.update(
-            {"class": "form-control"}
-        )  # Додаємо клас форми для стилізації
-
-    def clean_picture(self):
-        picture = self.cleaned_data.get("picture", None)
-        if picture:
-            if picture.size > 10 * 1024 * 1024:  # Перевірка розміру фотографії (10MB)
-                raise forms.ValidationError(
-                    "Фотографія занадто велика. Максимальний розмір - 10MB."
-                )
-        return picture
+from catalog.models import Author, Commentary
 
 
 class UserRegistrationForm(UserCreationForm):
     email = forms.EmailField(
         help_text="Required. Inform a valid email address.", required=True
     )
-    picture = forms.ImageField(label="Profile Picture", required=False)
 
     class Meta:
         model = Author
@@ -44,16 +17,24 @@ class UserRegistrationForm(UserCreationForm):
             "email",
             "password1",
             "password2",
-            "picture",  # Додайте поле picture до списку полів форми
+            "picture"
         ]
+
+    def clean_picture(self):
+        picture = self.cleaned_data.get('picture')
+        if picture:
+            if picture.size > 10 * 1024 * 1024:
+                raise forms.ValidationError("Фотографія занадто велика. Максимальний розмір - 10MB.")
+        return picture
 
     def save(self, commit=True):
         author = super(UserRegistrationForm, self).save(commit=False)
         author.email = self.cleaned_data["email"]
-        if "picture" in self.cleaned_data:
-            author.picture = self.cleaned_data["picture"]
+        if 'picture' in self.cleaned_data:
+            author.picture = self.cleaned_data['picture']
         if commit:
             author.save()
+        print(author.picture)
         return author
 
 
@@ -61,3 +42,27 @@ class CommentariesForm(forms.ModelForm):
     class Meta:
         model = Commentary
         fields = ["content"]
+
+
+class AuthorsForm(forms.ModelForm):
+    picture = forms.ImageField(required=False)
+
+    class Meta:
+        model = Author
+        fields = ["email",
+                  "mobile_phone",
+                  "years_of_experience",
+                  "picture"
+                  ]
+
+    def __init__(self, *args, **kwargs):
+        super(AuthorsForm, self).__init__(*args, **kwargs)
+        self.fields['picture'].widget.attrs.update({'class': 'form-control'})
+
+    def clean_picture(self):
+        picture = self.cleaned_data.get('picture')
+        if picture:
+            if picture.size > 10 * 1024 * 1024:
+                raise forms.ValidationError("Фотографія занадто велика. Максимальний розмір - 10MB.")
+        return picture
+
