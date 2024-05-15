8bf9be19259042e415d752610907f6204cb92048
Marina <marina.ua13@gmail.com>
2024-05-15 16:14:57 +0300

    add .env file, update requirements.txt

--- AntalyaInfoPoint/settings.py
+++ AntalyaInfoPoint/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 import os
 import dj_database_url
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +28,7 @@ SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["127.0.0.1", "antalya-info-point-9.onrender.com"]
 
